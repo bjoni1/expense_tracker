613310095a23ad31df8d9bbacac428a498525221
bjoni1 <bjonimerkuri1@gmail.com>
2024-05-13 00:35:17 +0200

    added the header and footer and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFooterComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
